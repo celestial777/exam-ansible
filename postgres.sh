262702ba058f4b50252c3e9523ed00208c963685
celestial777 <lovingmarvin@gmail.com>
2022-11-08 14:25:56 +0000

    exam-project

---
+++ postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt -y update && sudo apt -y upgrade
+
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+apt -y update
+
+apt -y install postgresql
